f9b500b0cb231ebcc8b689dc7b00e7778c464460
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2020-10-31 10:16:32 +0100

    Save preferences.

--- app/src/main/java/com/wohlmuth/onenote/NoteEditActivity.kt
+++ app/src/main/java/com/wohlmuth/onenote/NoteEditActivity.kt
@@ -6,13 +6,29 @@ import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_note_edit.*
 
 class NoteEditActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val preferences = Preferences()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_edit)
+        btnSave.setOnClickListener(this)
+
+        etTitle.setText(preferences.getNoteTitle(this))
+        etMessage.setText(preferences.getNoteMessage(this))
+    }
+
+    private fun saveNote() {
+        preferences.setNoteTitle(this, etTitle.text.toString())
+        preferences.setNoteMessage(this, etMessage.text.toString())
+        Toast.makeText(this, R.string.note_saved, Toast.LENGTH_LONG).show()
+        finish()
     }
 
     override fun onClick(v: View?) {
+        saveNote()
     }
 }
