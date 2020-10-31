4892eed0ba5d87eb504cea344dbfb7d82f1746b4
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2020-10-31 14:21:13 +0100

    Implemented View Holder Pattern.

--- app/src/main/java/com/wohlmuth/onenote/NoteAdapter.kt
+++ app/src/main/java/com/wohlmuth/onenote/NoteAdapter.kt
@@ -12,14 +12,41 @@ class NoteAdapter(context: Context): BaseAdapter() {
     private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = inflater.inflate(R.layout.list_item, parent, false)
-        val title = view.findViewById(R.id.tvTitle) as TextView
-        val message = view.findViewById(R.id.tvMessage) as TextView
-        val note: Note = getItem(position) as Note
+        val view: View
+        val holder: ViewHolder
 
-        title.text = note.title
-        message.text = note.message
+        // Check if the view already exists. If it does, there’s no need to inflate from the layout and call findViewById() again.
+        if (convertView == null) {
 
+            // If the view doesn’t exi st, you inflate the custom row layout from your XML.
+            view = inflater.inflate(R.layout.list_item, parent, false)
+
+            // Create a new ViewHolder with subviews initialized by using findViewById().
+            holder = ViewHolder()
+            holder.title = view.findViewById(R.id.tvTitle) as TextView
+            holder.message = view.findViewById(R.id.tvMessage) as TextView
+
+            // Hang onto this holder for future recycling by using setTag() to set the tag property of the view that the holder belongs to.
+            view.tag = holder
+        } else {
+
+            // Skip all the expensive inflation steps and just get the holder you already made.
+            view = convertView
+            holder = convertView.tag as ViewHolder
+        }
+
+        // Get relevant subviews of the row view.
+        val tvNoteTitle = holder.title
+        val tvNoteMessage = holder.message
+
+        // Get our note object for current position using getItem(position).
+        val note = getItem(position) as Note
+
+        // Set text on TextViews
+        tvNoteTitle.text = note.title
+        tvNoteMessage.text = note.message
+
+        // Return view containing all text values for current position
         return view
     }
 
@@ -35,4 +62,8 @@ class NoteAdapter(context: Context): BaseAdapter() {
         return 20
     }
 
+    private class ViewHolder {
+        lateinit var title: TextView
+        lateinit var message: TextView
+    }
 }
